d42f655ca3f535b8cee557babe906fcb06b6c1b5
Manohar Gella <manohargella272@gmail.com>
2023-07-16 05:05:44 +0530

    v 4

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main() {
     cout << "Manohar Gella" << endl;
     cout << "Compitetive programming notese" << endl;
     cout << "random change" << endl;
+    cout << "otehr change" << endl;
     return 0;
 }
