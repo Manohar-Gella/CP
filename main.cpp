ff5459698beb3e8c4ee1c7fbe7eee89476892ea5
Manohar Gella <manohargella272@gmail.com>
2023-07-17 01:40:48 +0530

    v 5

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main() {
     cout << "Compitetive programming notese" << endl;
     cout << "random change" << endl;
     cout << "otehr change" << endl;
+    cout << "lets do icpc" << endl;
     return 0;
 }
