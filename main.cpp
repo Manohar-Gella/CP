4a001ee66b0c070674929a122efb77fcf76e665b
Manohar Gella <manohargella272@gmail.com>
2023-07-17 01:43:25 +0530

    v 6

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+
+#include <iostream>
+#include <string>
 # include <bits/stdc++.h>
 using namespace std;
 
@@ -7,5 +10,26 @@ int main() {
     cout << "random change" << endl;
     cout << "otehr change" << endl;
     cout << "lets do icpc" << endl;
+    std::string name;
+    int age;
+    std::string email;
+
+    // Form input
+    std::cout << "Please fill in the form:\n";
+    std::cout << "Name: ";
+    std::getline(std::cin, name);
+
+    std::cout << "Age: ";
+    std::cin >> age;
+    std::cin.ignore(); // Ignore the newline character in the input buffer
+
+    std::cout << "Email: ";
+    std::getline(std::cin, email);
+
+    // Form submission
+    std::cout << "\nForm submitted successfully!\n";
+    std::cout << "Name: " << name << "\n";
+    std::cout << "Age: " << age << "\n";
+    std::cout << "Email: " << email << "\n";
     return 0;
 }
