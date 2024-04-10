faef9cc5c9f1a14c3500cfa6d2b6273833c5e656
Manohar Gella <manohargella272@gmail.com>
2024-04-10 23:29:03 +0530

    Rebase

--- lessons/1structure.cpp
+++
@@ -1,7 +0,0 @@
-// Basic Structure of C++
-#include <iostream>
-
-int main() {
-    // The code goes here
-    return 0;
-}
---
+++ lessons/C++/1-structure.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+
+int main() {
+    std::cout<<"Manohar Gella";
+    return 0;
+}
