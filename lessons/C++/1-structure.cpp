693c25d937aa440db611a8fb1b560347f1fadc6c
Manohar Gella <manohargella272@gmail.com>
2024-04-10 23:39:37 +0530

    Basic structure of C

--- lessons/C++/1-structure.cpp
+++ lessons/C++/1-structure.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // head file input output stream, it accept input and output
+using namespace std; // to avoid std:: in every line of code
 
-int main() {
-    cout<<"Manohar Gella"<<endl;
-    return 0;
+int main() { // main function
+    cout<<"Manohar Gella"<< endl; // to print out any string
+    return 0; // to return 0 on complition
 }
