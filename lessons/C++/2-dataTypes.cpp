24dc32e5cee280954f9c94bf1367113edbb65908
Manohar Gella <manohargella272@gmail.com>
2024-04-11 01:06:33 +0530

    Data Types

---
+++ lessons/C++/2-dataTypes.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*
+     FORMAT
+     dataType variableName = data; <- ; stands for terminator of the line
+    */
+
+
+   // interger
+    int age = 18;
+    // double for large numbers
+    double weight = 300.43;
+    // cahr to store single character
+    char grade = 'a';
+    // boolean
+    bool married = false;
+    bool virgin = true;
+    bool hadGirlfriend = 0;
+    bool introvert = 1;
+
+
+    cout << age << endl;
+    cout << weight << endl;
+    cout << grade << endl;
+    cout << married << endl;
+    cout << virgin << endl;
+    cout << hadGirlfriend << endl;
+    cout << introvert << "\n";
+    return 0;
+}
