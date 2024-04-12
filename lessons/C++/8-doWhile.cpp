6c080aa15947474e769640b64b49cff67a0963df
Manohar Gella <manohargella272@gmail.com>
2024-04-13 02:29:00 +0530

    Do while

---
+++ lessons/C++/8-doWhile.cpp
@@ -0,0 +1,20 @@
+// structure of do while in C++
+//  do {
+//         code to execute until
+//         runs code one despite of the condition
+//  }while(condition);
+
+#include <iostream>
+using namespace std;
+int main() {
+   int i = 1;
+   char name[50];
+   do{
+       cout << "Enter your name: ";
+       cin.getline(name, 50);
+       cout << "Hello " << name << endl;
+       cout << i << endl;
+       i++;
+   }while(i<=5);
+    return 0;
+}
