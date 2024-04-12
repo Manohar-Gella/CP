c6dcf40eaa3482c3e55b646c0d44b6329e50655d
Manohar Gella <manohargella272@gmail.com>
2024-04-13 02:09:43 +0530

    While Loops

---
+++ lessons/C++/7-While.cpp
@@ -0,0 +1,24 @@
+// while loop in c++
+
+// structire of wihile loop
+// while(conditon){
+//     codto execute until the condtion gets satisfies
+// }
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 0;
+    while (i<=10)
+    {
+        cout << i << endl;
+        i++;
+    }
+    
+    return 0;
+}
+
+
+// end
