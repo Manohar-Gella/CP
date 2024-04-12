a78491d21eb752f4d283f86e4d0240c99ed069d4
Manohar Gella <manohargella272@gmail.com>
2024-04-13 02:22:24 +0530

    while loop

--- lessons/C++/7-While.cpp
+++ lessons/C++/7-While.cpp
@@ -22,3 +22,24 @@ int main() {
 
 
 // end
+
+// multiplication table using while loop
+
+#include <iostream>
+using namespace std;
+int main() {
+    int i = 1;
+    int x;
+    int t;
+    cout << "Which table do you want: ";
+    cin >> x;
+    cout << "What times: ";
+    cin >> t;
+    while(i<=t){
+        cout << x << " * " << i << " = " << x*i << endl;
+        i++;
+    }
+    
+    return 0;
+}
+
